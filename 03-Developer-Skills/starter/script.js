16740714d51011468edd1e6fd3f18689eedfd988
Richard A <richard@main-vision.com>
2022-03-30 17:33:01 +0200

    code snippets

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,14 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+const x = 23;
+
+const y = 42;
+
+if (x === 42) {
+  console.log("Yes, that's the answer!");
+}
+
+const calcAge = birthYear => 2037 - birthYear;
+
+console.log();
