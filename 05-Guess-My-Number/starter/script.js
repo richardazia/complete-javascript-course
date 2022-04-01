832f4668c02a3ef928e5257a73467b9958a32347
Richard A <richard@main-vision.com>
2022-04-01 17:48:28 +0200

    add not guess response

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,11 +1,20 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = 'You Win!';
-console.log(document.querySelector('.message').textContent);
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = 'You Win!';
+// console.log(document.querySelector('.message').textContent);
 
-document.querySelector('.number').textContent = 19;
-document.querySelector('.score').textContent = 16;
+// document.querySelector('.number').textContent = 19;
+// document.querySelector('.score').textContent = 16;
 
-document.querySelector('.guess').value = '23';
-console.log((document.querySelector('.guess').value = '23'));
+// document.querySelector('.guess').value = '23';
+// console.log((document.querySelector('.guess').value = '23'));
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number entered';
+  }
+});
