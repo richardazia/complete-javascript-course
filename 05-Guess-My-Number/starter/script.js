b3f993c50041335eb24736534f332233ebe3d8dd
Richard A <richard@main-vision.com>
2022-04-01 16:49:34 +0200

    Add query selectors

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'You Win!';
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.number').textContent = 19;
+document.querySelector('.score').textContent = 16;
+
+document.querySelector('.guess').value = '23';
+console.log((document.querySelector('.guess').value = '23'));
