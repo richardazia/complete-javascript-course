9a3b8f3751fe33d5f6ab3fd88ad761413aff2be6
Richard A <richard@main-vision.com>
2022-04-03 10:01:48 +0200

    two minor changes

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -10,8 +10,11 @@
 // document.querySelector('.guess').value = '23';
 // console.log((document.querySelector('.guess').value = '23'));
 
+// There is a bug in the way this app counts the score after it has been reset
+
 var number = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highScore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -25,6 +28,12 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = 'You Win!';
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
+
     // Too high
   } else if (guess > number) {
     if (score > 0) {
@@ -49,12 +58,13 @@ document.querySelector('.check').addEventListener('click', function () {
 });
 
 document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.number').style.width = '15rem';
   document.querySelector('.number').textContent = '??';
-  document.querySelector('.score').textContent = 20;
+  document.querySelector('.score').textContent = score;
+  console.log('score:' + score);
   document.querySelector('.message').textContent = 'Make your Guess!';
   document.querySelector('.guess').value = '';
   number = Math.trunc(Math.random() * 20) + 1;
-  console.log(number);
 });
