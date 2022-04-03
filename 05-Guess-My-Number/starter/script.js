197d3432752d148264807287d49c789df91e3a21
Richard A <richard@main-vision.com>
2022-04-03 11:03:13 +0200

    refactored number guessing app

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,31 +1,22 @@
 'use strict';
 
-// console.log(document.querySelector('.message').textContent);
-// document.querySelector('.message').textContent = 'You Win!';
-// console.log(document.querySelector('.message').textContent);
-
-// document.querySelector('.number').textContent = 19;
-// document.querySelector('.score').textContent = 16;
-
-// document.querySelector('.guess').value = '23';
-// console.log((document.querySelector('.guess').value = '23'));
-
-// There is a bug in the way this app counts the score after it has been reset
-
 var number = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highScore = 0;
 
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
-  console.log(guess, typeof guess);
   // When the input is not a number, or blank
   if (!guess) {
-    document.querySelector('.message').textContent = 'No number entered';
+    displayMessage('Please enter a number');
     // When the person has guessed correctly
   } else if (guess === number) {
     document.querySelector('.number').textContent = number;
-    document.querySelector('.message').textContent = 'You Win!';
+    displayMessage('You Win!');
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
 
@@ -33,25 +24,13 @@ document.querySelector('.check').addEventListener('click', function () {
       highScore = score;
       document.querySelector('.highscore').textContent = highScore;
     }
-
-    // Too high
-  } else if (guess > number) {
-    if (score > 0) {
-      document.querySelector('.message').textContent = 'Too high';
+  } else if (guess !== number) {
+    if (score > 1) {
+      displayMessage(guess > number ? 'Too high' : 'Too low');
       document.querySelector('.score').textContent = score--;
     } else {
       // When the person loses
-      document.querySelector('.message').textContent = 'You Lose!';
-      document.querySelector('.score').textContent = 0;
-    }
-    // Tow low
-  } else if (guess < number) {
-    if (score > 0) {
-      document.querySelector('.message').textContent = 'Too Low';
-      document.querySelector('.score').textContent = score--;
-    } else {
-      // When the person loses
-      document.querySelector('.message').textContent = 'You Lose!';
+      displayMessage('You Lose!');
       document.querySelector('.score').textContent = 0;
     }
   }
@@ -63,8 +42,7 @@ document.querySelector('.again').addEventListener('click', function () {
   document.querySelector('.number').style.width = '15rem';
   document.querySelector('.number').textContent = '??';
   document.querySelector('.score').textContent = score;
-  console.log('score:' + score);
-  document.querySelector('.message').textContent = 'Make your Guess!';
+  displayMessage('Start Guessing...');
   document.querySelector('.guess').value = '';
   number = Math.trunc(Math.random() * 20) + 1;
 });
