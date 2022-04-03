b43586ed9274c4e3d6d40e1ed847812b07bd3ec2
Richard A <richard@main-vision.com>
2022-04-03 09:14:22 +0200

    challenge attempt

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -10,10 +10,8 @@
 // document.querySelector('.guess').value = '23';
 // console.log((document.querySelector('.guess').value = '23'));
 
-const number = Math.trunc(Math.random() * 20) + 1;
+var number = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-
-document.querySelector('.number').textContent = number;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -23,6 +21,7 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = 'No number entered';
     // When the person has guessed correctly
   } else if (guess === number) {
+    document.querySelector('.number').textContent = number;
     document.querySelector('.message').textContent = 'You Win!';
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
@@ -47,4 +46,14 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = 0;
     }
   }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.number').textContent = '??';
+  document.querySelector('.score').textContent = 20;
+  document.querySelector('.message').textContent = 'Make your Guess!';
+  number = Math.trunc(Math.random() * 20) + 1;
+  console.log(number);
 });
