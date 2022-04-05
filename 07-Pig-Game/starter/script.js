b1c63c556d04a5224b549f73ffa633c79db9968c
Richard A <richard@main-vision.com>
2022-04-05 11:55:45 +0200

    individual attempt

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,5 +1,7 @@
 'use strict';
 // Grab dom elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
@@ -15,7 +17,10 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
+let currentPlayer = 0;
+let scoreHold = 0;
 
 // Dice Rolling functions
 btnRoll.addEventListener('click', function () {
@@ -28,9 +33,49 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     // add dice to currentScore
     currentScore += dice;
-    current0El.textContent = currentScore;
+    document.getElementById(`current--${currentPlayer}`).textContent =
+      currentScore;
+    // Use Hold button
   } else {
     // reset currentScore to 0 for current player
-    // switch to next player
+    document.getElementById(`current--${currentPlayer}`).textContent = 0;
+    currentScore = 0;
+    currentPlayer = currentPlayer === 0 ? 1 : 0;
+    scoreHold = scoreHold === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
+  // Reset function
+  btnNew.addEventListener('click', function () {
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+    diceEl.classList.add('hidden');
+  });
+
+  // Winning condition
+  if (current0El.textContent >= 100) {
+    alert('Player 1 wins!');
+  } else if (current1El.textContent >= 100) {
+    alert('Player 2 wins!');
+  }
+});
+
+btnHold.addEventListener('click', function () {
+  // add currentScore to player's score
+
+  if (scoreHold !== 0) {
+    document.getElementById(`score--${currentPlayer}`).textContent = Number(
+      currentScore + `${scoreHold}`
+    );
+  } else {
+    currentScore = scoreHold;
+    document.getElementById(`score--${currentPlayer}`).textContent = scoreHold;
+  }
+  currentPlayer = currentPlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+  // reset currentScore to 0 for current player
+  // switch to next player
 });
