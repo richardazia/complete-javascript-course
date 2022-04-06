a7ee4601188ac892acea09adf1b514da7f821709
Richard A <richard@main-vision.com>
2022-04-06 11:16:55 +0200

    change user functionality

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -20,62 +20,69 @@ diceEl.classList.add('hidden');
 const scores = [0, 0];
 let currentScore = 0;
 let currentPlayer = 0;
-let scoreHold = 0;
+let playing = true;
 
-// Dice Rolling functions
-btnRoll.addEventListener('click', function () {
-  // Generate random number between 1-6 for dice roll
-  const dice = Math.trunc(Math.random() * 6) + 1;
-  // display the result
-  diceEl.classList.remove('hidden');
-  diceEl.src = `dice-${dice}.png`;
-  // Check if the number was 1 or other
-  if (dice !== 1) {
-    // add dice to currentScore
-    currentScore += dice;
-    document.getElementById(`current--${currentPlayer}`).textContent =
-      currentScore;
-    // Use Hold button
-  } else {
-    // reset currentScore to 0 for current player
-    document.getElementById(`current--${currentPlayer}`).textContent = 0;
-    currentScore = 0;
-    currentPlayer = currentPlayer === 0 ? 1 : 0;
-    scoreHold = scoreHold === 0 ? 1 : 0;
-    player0El.classList.toggle('player--active');
-    player1El.classList.toggle('player--active');
-  }
-  // Reset function
-  btnNew.addEventListener('click', function () {
-    score0El.textContent = 0;
-    score1El.textContent = 0;
-    current0El.textContent = 0;
-    current1El.textContent = 0;
-    diceEl.classList.add('hidden');
-  });
-
-  // Winning condition
-  if (current0El.textContent >= 100) {
-    alert('Player 1 wins!');
-  } else if (current1El.textContent >= 100) {
-    alert('Player 2 wins!');
-  }
-});
-
-btnHold.addEventListener('click', function () {
-  // add currentScore to player's score
-
-  if (scoreHold !== 0) {
-    document.getElementById(`score--${currentPlayer}`).textContent = Number(
-      currentScore + `${scoreHold}`
-    );
-  } else {
-    currentScore = scoreHold;
-    document.getElementById(`score--${currentPlayer}`).textContent = scoreHold;
-  }
+const switchPlayer = function () {
+  document.getElementById(`current--${currentPlayer}`).textContent = 0;
+  currentScore = 0;
   currentPlayer = currentPlayer === 0 ? 1 : 0;
   player0El.classList.toggle('player--active');
   player1El.classList.toggle('player--active');
-  // reset currentScore to 0 for current player
-  // switch to next player
+};
+
+// Dice Rolling functions
+btnRoll.addEventListener('click', function () {
+  if (playing) {
+    // Generate random number between 1-6 for dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    // display the result
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
+    // Check if the number was 1 or other
+    if (dice !== 1) {
+      // add dice to currentScore
+      currentScore += dice;
+      document.getElementById(`current--${currentPlayer}`).textContent =
+        currentScore;
+      // Use Hold button
+    } else {
+      // reset currentScore to 0 for current player
+      switchPlayer();
+    }
+  }
+});
+// Reset function
+btnNew.addEventListener('click', function () {
+  playing = true;
+  document
+    .querySelector(`.player--${currentPlayer}`)
+    .classList.remove('player--winner');
+  switchPlayer();
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  diceEl.classList.add('hidden');
+});
+
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    // Add currentScore to  player score
+    scores[currentPlayer] += currentScore;
+    document.getElementById(`score--${currentPlayer}`).textContent =
+      scores[currentPlayer];
+    // 2 Check if player >= 100
+    // Winning condition
+    if (scores[currentPlayer] >= 15) {
+      playing = false;
+      document
+        .querySelector(`.player--${currentPlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${currentPlayer}`)
+        .classList.remove('player--active');
+    } else {
+      switchPlayer();
+    }
+  }
 });
